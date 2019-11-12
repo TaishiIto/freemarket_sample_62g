24502d62fee1c50fe2eb1b694bc9dfe5d6b3cb90
iWonder118 <52240372+iWonder118@users.noreply.github.com>
2019-11-12 12:23:14 +0900

    fixed-basic-auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth, if: :production? 
+  before_action :basic_auth, if: :production?
   protect_from_forgery with: :exception
 
   private
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   def production?
     Rails.env.production?
   end
-  
+
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
