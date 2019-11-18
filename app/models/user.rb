6e930605d633dfafdba36275c2075ad3ee2eb6f5
ono2342 <56573437+ono2342@users.noreply.github.com>
2019-11-18 20:28:22 +0900

    add model/create device controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,27 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: %i[google_oauth2 facebook]
 
   has_one :address
   has_many :items_statuses
   has_many :items, through: :items_statuses
   accepts_nested_attributes_for :address
 
+  def self.from_omniauth(auth)
+    user = User.where(uid: auth.uid, provider: auth.provider).first
+
+    unless user
+      user = User.create(
+        uid:      auth.uid,
+        provider: auth.provider,
+        name:     auth.info.name,
+        email:    auth.info.email,
+        password: Devise.friendly_token[0, 20]
+      )
+    end
+
+    user
+  end
 end
