2a626b47f4869ab7152c1dbf87d006417dd4a6d3
iWonder118 <52240372+iWonder118@users.noreply.github.com>
2019-11-19 20:17:15 +0900

    fixed-setting

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,16 +1,22 @@
-if Rails.env.production?
-  CarrierWave.configure do |config|
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
+CarrierWave.configure do |config|
+  if Rails.env.production? # 本番環境:AWS使用
+    config.storage = :fog
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
       aws_secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
-      #S3のリージョン #ap-northeast-1はアジアパシフィック(東京)
-      region: 'ap-northeast-1'
+      region: 'ap-northeast-1' 
     }
-    # S3のバケット名
-    config.fog_directory  = 'freemarket62g'
-    # S3に保存しておく期間
-    config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
-  end
+    config.fog_directory  = 'freemarket62g' #S3のバケット名
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarket62g'
+  else
+    config.storage :file # 開発環境:public/uploades下に保存
+    config.enable_processing = false if Rails.env.test? #test:処理をスキップ
+  end  
+  CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/ 
 end
