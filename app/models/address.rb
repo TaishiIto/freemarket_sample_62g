3805359b01afa652cd3e72335fa4c3d61647f5ef
ono2342 <56573437+ono2342@users.noreply.github.com>
2019-11-21 20:01:50 +0900

    varidation

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,4 +2,11 @@ class Address < ApplicationRecord
   belongs_to :user
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
+
+  validates zip_code:, presence: true
+  validates city:, presence: true
+  validates house_number:, presence: true
+  
+  validates prefecture_id:, presence: true
+  validates user_id:, presence: true
 end
