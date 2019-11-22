235d52003d93d8e082f9bbe61d3ce7657a7cbf85
ono2342 <56573437+ono2342@users.noreply.github.com>
2019-11-22 16:15:54 +0900

    change model address

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,10 +3,10 @@ class Address < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
 
-  validates zip_code:, presence: true
-  validates city:, presence: true
-  validates house_number:, presence: true
-  
-  validates prefecture_id:, presence: true
-  validates user_id:, presence: true
+  validates :zip_code, presence: true
+  validates :city, presence: true
+  validates :house_number, presence: true
+
+  validates :prefecture_id, presence: true
+  validates :user_id, presence: true
 end
