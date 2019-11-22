bcd2b48565e7538a64d4657cfa0f014dcd9f61b0
iWonder118 <52240372+iWonder118@users.noreply.github.com>
2019-11-22 17:20:42 +0900

    itemlist-function(投稿順10件分)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
+
   def index
-    @items = Item.all
+    @items = Item.includes(:items_statuses).limit(10).order("created_at DESC")
   end
 
   def new
