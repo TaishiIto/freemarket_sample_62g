01a709df63ef3f6bc1a98c993b95def66a121c1a
iWonder118 <52240372+iWonder118@users.noreply.github.com>
2019-11-24 15:26:26 +0900

    correction-update-action

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,8 +26,11 @@ class ItemsController < ApplicationController
   end
 
   def update
-    @detail = Item.update(item_params)
-    redirect_to root_path
+    if @detail.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   def destroy
