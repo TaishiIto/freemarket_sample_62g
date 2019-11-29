4ff60552f05818b7043854238ee65452b2271051
iWonder118 <52240372+iWonder118@users.noreply.github.com>
2019-11-29 15:20:21 +0900

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_27_103501) do
-
+ActiveRecord::Schema.define(version: 2019_11_27_105800) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
