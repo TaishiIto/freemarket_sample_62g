a6af05484b295973027e415e3d6b2e3412330c70
iWonder118 <52240372+iWonder118@users.noreply.github.com>
2019-12-03 15:33:03 +0900

    add-images-validation

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,7 @@ class Item < ApplicationRecord
   has_one  :delivery,dependent: :destroy
   accepts_nested_attributes_for :delivery
 
+  validate :images_presence
   validates :name, :description, presence: true
   validates :price,       presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
   validates :condition,   numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 6}
@@ -17,4 +18,14 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :size
   belongs_to_active_hash :category
+
+  def images_presence
+    if images.attached?
+      if !images.content_type.in?(%('image/jpeg image/png'))
+        errors.add(:images, 'にはjpegまたはpngファイルを添付してください')
+      end
+    else
+      errors.add(:images, 'ファイルを添付してください')
+    end
+  end
 end
