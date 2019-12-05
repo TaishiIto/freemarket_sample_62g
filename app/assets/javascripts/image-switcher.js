1c7ba70bcd5e70560ad7c438afe0680f85de6950
iWonder118 <52240372+iWonder118@users.noreply.github.com>
2019-12-05 20:38:24 +0900

    removed-turbolinks

--- app/assets/javascripts/image-switcher.js
+++ app/assets/javascripts/image-switcher.js
@@ -1,9 +1,7 @@
-$(document).on('turbolinks:load', function() {
-  $(function(){
-    $('.thumbnai').click(function(){
-      var $thisImg = $(this).attr('src');
-      var $thisAlt = $(this).attr('alt');
-      $('#main-picture').attr({src:$thisImg,alt:$thisAlt});
-    });
+$(function(){
+  $('.thumbnai').click(function(){
+    var $thisImg = $(this).attr('src');
+    var $thisAlt = $(this).attr('alt');
+    $('#main-picture').attr({src:$thisImg,alt:$thisAlt});
   });
 });
